b644049575f73001dcbca00a5e7f4cdfd29d360f
gianellin <g.cevallos@gianellin.com>
2022-11-15 12:07:41 -0500

    all buttons work

--- js/main.js
+++ js/main.js
@@ -1,17 +1,21 @@
 
+
+
+
 // 1 These are the options for the slot
 
 const items = ["🎩", "🏡", "😑", "🦄"];
 
 
-
-
 const button =document.querySelector('#play')
 button.addEventListener('click', play);
 
+const button2 =document.querySelector('#reset')
+button2.addEventListener('click', init);
+
 function getRandomSlotValues() {
     // is to randomly return
-    // 'slot 1' 'slot 2' 'slot3' values "emojis"
+    // 'slot 1' 'slot 2' 'slot3' with randon values = out the "emojis"
     const randomItem = Math.floor(Math.random()*items.length);
     return items[randomItem]
 }
@@ -19,30 +23,32 @@ function getRandomSlotValues() {
 function play (){
     console.log("button is working")
 
-    //randomly generate a value for the 3 slots
-    
-
+    //randomly generate a value for the 3 slots 
     slotsValue.slot1 = getRandomSlotValues();
     slotsValue.slot2 = getRandomSlotValues();
     slotsValue.slot3 = getRandomSlotValues();
 
-    if (slotsValue.slot1 === slotsValue.slot2 && slotsValue.slot1 === slotsValue.slot3) {
-        alert("Jackpot! PLAYER WON $$$$$")
-    } else {
-        console.log('tries add, but nothing to the score')
-    }
+    // Place the loosing and winning condition
     //then update the  state (scores and tries) with that value
-
-
+    if (slotsValue.slot1 === slotsValue.slot2 && slotsValue.slot1 === slotsValue.slot3) {
+        scores.player +=1;
+        scores.tries +=1;
+        console.log('YOU WON $$$$$!')
+       
+    } else {
+        scores.tries +=1;
+        console.log('You lost, try again!')
+    }
+    
     // AT THE END OF EVERY CONTROLLER FUNCTION CALL RENDR
     render()
 }
 
 // define our state variable
 // how would you define the score state
-let slotsValue; // stores the players dial
+let slotsValue; // stores the players dial or
 let scores; // stores the scores
-let winner; //
+
 
 //CACHED ELEMENTS
 //The elements (HTML) we're going to update repeatedly
@@ -61,6 +67,8 @@ const slotsValueEls = {
 
 //The init function should be called when the page loads
 // or when we want to reset the game.
+//Initialize means score = 0, tries =0  and the slot1, slot2, slot3 equal 'X'
+
 init();
 //init controller 
 function init(){ // controller function is updating our state
@@ -77,49 +85,31 @@ function init(){ // controller function is updating our state
         slot3: 'X'
     };
 
-    
-
-    render(); // take out state variables;
-    // and represent visually on the view in some manor
+    render(); 
+    // take out state variables;
+    // and represent visually on the view in some manner
     //slotsValue.slot1 , slotsValue.slot1 , slotsValue.slot1 will be represented by a emoji/string
     //AKA update the DOM
-    // gets called at the end of every controller function
+    // render() gets called at the end of every controller function
 }
-
-//Initialize means score = 0, tries =0  and the slot1, slot2, slot3 equal 'X'
 
 
 function render (){
 
     //UPDATE THE DOM to visually represent 
     //our state variables
-
-    
-
     
     for (let key in scores) {
         scoresEls[key].innerText = scores[key];
     }
 
-
     slotsValueEls.slot1.innerText = slotsValue.slot1
     slotsValueEls.slot2.innerText = slotsValue.slot2
     slotsValueEls.slot3.innerText = slotsValue.slot3
     
-    // for (let key in slotValue) {
-    //     slotsValueEls[key].innerText = slotsValue[key]
-
-
-    // }
-
-    // OR we can use the following notation:
-    // scoresEls.player.innerText = scores.player;
-    // scoresEls.tries.innerText= scores.tries;
 }
 
 
-
-// this part select a random number from the array
 
 
 
